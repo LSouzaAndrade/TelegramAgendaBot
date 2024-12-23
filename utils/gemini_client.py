8c2cfc6d7af08b77d37522e70fe5277628c99eb0
Lucas Souza <lucas.souza.andrade66@gmail.com>
2024-12-22 21:13:58 -0300

    Update gemini_client.py

--- utils/gemini_client.py
+++ utils/gemini_client.py
@@ -4,18 +4,20 @@ from dotenv import load_dotenv
 from utils.gemini_function_calling import tools_parameters
 from datetime import datetime
 
+
 load_dotenv()
 GOOGLE_GEMINI_API_KEY = os.environ['GOOGLE_GEMINI_API_KEY']
 genai.configure(api_key=GOOGLE_GEMINI_API_KEY)
 
 model_name = 'models/gemini-1.5-flash'
 tool_config, tools = tools_parameters('auto')
-system_instruction = f'''You are part of a reservation system API.
-                        Your task is to schedule objects as requested. 
-                        Do not perform any other tasks.
-                        Do not make assumptions; always ask for any missing information.
-                        Always validate the information with the user before making the function call.
-                        Today's date is {datetime.now().strftime('%d-%m-%Y')}'''
+system_instruction = f'''Você faz parte de uma API de sistema de reservas.
+                         Sua tarefa é agendar objetos conforme solicitado.
+                         Não realize nenhuma outra tarefa ou fale sobre outros assuntos.
+                         Não faça suposições.
+                         Sempre pergunte por qualquer informação ausente, uma por vez.
+                         Sempre valide as informações com o usuário antes de realizar a chamada da função.
+                         Hoje é: {datetime.now().strftime('%d-%m-%Y')}'''
 
 def initialize_chat_instance(model_name=model_name, tools=tools, system_instruction=system_instruction):
     model = genai.GenerativeModel(model_name=model_name, tools=tools, system_instruction=system_instruction)
