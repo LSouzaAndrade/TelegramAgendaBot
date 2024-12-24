f26bc51733ad9908536f74471e699eec36b53d3d
Lucas Souza <lucas.souza.andrade66@gmail.com>
2024-12-24 16:52:05 -0300

    Update telegram_bot.py

--- utils/telegram_bot.py
+++ utils/telegram_bot.py
@@ -4,10 +4,10 @@ from telethon import TelegramClient, events
 
 
 load_dotenv()
-TELEGRAM_API_ID = os.environ['TELEGRAM_API_ID']
-TELEGRAM_API_HASH = os.environ['TELEGRAM_API_HASH']
-TELEGRAM_BOT_TOKEN = os.environ['TELEGRAM_BOT_TOKEN']
-TELEGRAM_GROUP_CHAT_ID = os.environ['TELEGRAM_GROUP_CHAT_ID']
+TELEGRAM_API_ID = os.environ.get('TELEGRAM_API_ID')
+TELEGRAM_API_HASH = os.environ.get('TELEGRAM_API_HASH')
+TELEGRAM_BOT_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
+TELEGRAM_GROUP_CHAT_ID = os.environ.get('TELEGRAM_GROUP_CHAT_ID')
 
 def get_telegram_client():
     client = TelegramClient('message_listener', api_id=TELEGRAM_API_ID, api_hash=TELEGRAM_API_HASH)
