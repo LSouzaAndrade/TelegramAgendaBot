9a9ef07e70d416e2cd87a77463414a1fd0aa3694
Lucas Souza <lucas.souza.andrade66@gmail.com>
2024-12-24 16:52:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-from utils.gemini_client import get_chat_response
+from utils.GPT_API_handler import ChatInstance
 from utils.telegram_bot import *
+from utils.Tool_functions import function_map, tools
 
 
+model = 'gpt-4o-mini'
 client = get_telegram_client()
+chat = ChatInstance(model, tools=tools, function_map=function_map)
 
 @client.on(events.NewMessage())
 async def listener(event):
     if event.chat_id == int(TELEGRAM_GROUP_CHAT_ID) and event.text != '':
-        response = get_chat_response(event.text)
+        response = chat.send_message(event.text)
         await event.reply(response)
 
 client.start(bot_token=TELEGRAM_BOT_TOKEN)
